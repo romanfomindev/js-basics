c2cfd10fc3d3eb553aa11acbdecc00125dde809b
romanfomindev <rfomin1984@gmail.com>
2026-01-02 16:05:02 +0300

    fix

--- 8-crypto/main.js
+++ 8-crypto/main.js
@@ -1,18 +1,19 @@
 // Функция шифрования пароля
+// Функция шифрования пароля - переворачивает строку
 function crypto(password) {
     // 1. Разбиваем пароль на массив символов
     let chars = password.split('');
 
-    // 2. Меняем местами символы попарно (1-й со 2-м, 3-й с 4-м и т.д.)
-    for (let i = 0; i < chars.length - 1; i += 2) {
-        // Временная переменная для обмена
-        let temp = chars[i];
-        chars[i] = chars[i + 1];
-        chars[i + 1] = temp;
+    // 2. Создаем новый массив для результата
+    let result = [];
+
+    // 3. Переворачиваем строку - добавляем символы с конца
+    for (let i = chars.length - 1; i >= 0; i--) {
+        result.push(chars[i]);
     }
 
-    // 3. Склеиваем массив обратно в строку
-    return chars.join('');
+    // 4. Склеиваем массив обратно в строку
+    return result.join('');
 }
 
 // Функция проверки пароля
@@ -20,16 +21,15 @@ function check(encrypted, password) {
     // 1. Разбиваем зашифрованный пароль на символы
     let chars = encrypted.split('');
 
-    // 2. Воспроизводим алгоритм назад (меняем местами обратно)
-    for (let i = 0; i < chars.length - 1; i += 2) {
-        // Временная переменная для обмена
-        let temp = chars[i];
-        chars[i] = chars[i + 1];
-        chars[i + 1] = temp;
+    // 2. Переворачиваем обратно
+    let result = [];
+
+    for (let i = chars.length - 1; i >= 0; i--) {
+        result.push(chars[i]);
     }
 
     // 3. Склеиваем в строку
-    let decrypted = chars.join('');
+    let decrypted = result.join('');
 
     // 4. Сравниваем с переданным паролем
     return decrypted === password;
